da088f3b43468c6693f1d53096e3c2103c2f4aa5
Anthony Previte <anthonyprevite@Anthonys-MBP-4.lan>
2024-11-20 15:42:09 -0500

    establishing framework

---
+++ src/app/manager/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const Manager: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the New Page!</h1>
+      <p>This is the content of your new page.</p>
+    </div>
+  );
+};
+
+export default Manager;
