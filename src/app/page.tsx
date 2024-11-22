aa4493b58e23cd83aab638eed380b6cfae61100d
Vic <vjradina@wpi.edu>
2024-11-22 13:07:13 -0500

    login changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,13 @@ const HomePage: React.FC = () => {
 
   const handleLogin = (which: number) => {
     if (which === 0){
+      //search x database
       router.push('/consumer');
     }else if (which === 1){
+      //search y database
       router.push('/manager');
     }else if (which === 2){
+      //search z database
       router.push('/admin');
     }
   };
@@ -21,9 +24,13 @@ const HomePage: React.FC = () => {
   return (
     <div>
       <h1>Log in screen</h1>
-      <button onClick={() => handleLogin(0)}>Go to Consumer button </button>
-      <button onClick={() => handleLogin(1)}>Go to Manager button </button>
-      <button onClick={() => handleLogin(2)}>Go to Admin button </button>
+      <button className='login_button_container' onClick={() => handleLogin(0)}> Consumer </button>
+      <button className='login_button_container' onClick={() => handleLogin(1)}> Manager </button>
+      <button className='login_button_container' onClick={() => handleLogin(2)}> Admin</button>
+      
+      <input className='input_container'type="email" id='username' ></input>
+      <label>Password</label>
+      <input className='input_container' type='password'></input>
     </div>
   );
 };
