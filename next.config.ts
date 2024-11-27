d4619c5bcdc5218d408a9a5fcaf966cfdf619861
Vic <vjradina@wpi.edu>
2024-11-27 03:59:16 -0500

    next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: 'softEng2024_Table4u/'
 };
 
 export default nextConfig;
